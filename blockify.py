9b3c25934915d51bbccb55d22398549df5f991da
Amy Zhou <amy_zhou@college.harvard.edu>
2020-03-12 01:34:30 -0400

    allows for multiple projects

--- blockify.py
+++ blockify.py
@@ -4,17 +4,33 @@ import io
 import os
 import argparse
 
+def get_ids_from_file(filename):
+    """Given a filename, return a list of IDs from a newline-separated file. Project/studio link agnostic."""
+    ids = list()
+    try:
+        ids = list()
+        with open(filename) as f:
+            for l in f.readlines():
+                ids.append(helpers.get_id(l))
+    except:
+        pass
+    return ids
+
 def get_project_ids(arguments):
     """Given input arguments, return a set of all the project IDs."""
     projects = list()
     if arguments.project is not None:
         projects = arguments.project
+    elif arguments.project_list is not None:
+        for each in arguments.project_list:
+            projects.append(each)
     return set(projects)
 
 def blockify(jsonfiles, output_directory=os.getcwd(), file_name=None):
     """Given project IDs, loads the JSONs to list the blocks"""
+    if len(jsonfiles) < 1:
+        raise RuntimeError("Try again with a valid format for the Project ID")
     for jsonfile in jsonfiles:
-        print("jsonfile", jsonfile)
         block_dict = {}
         block_list = []
         with open(jsonfile +'.json') as f:
@@ -34,17 +50,14 @@ def blockify(jsonfiles, output_directory=os.getcwd(), file_name=None):
         block_file = open(jsonfile + "_blocks.txt", "w")
         block_file.writelines(block_list)
         block_file.close()       
-    return block_list, block_dict      
 
 def get_arguments():
     parser = argparse.ArgumentParser(description="List blocks in Scratch project.")
 
     # Arguments related to input
     inputs = parser.add_mutually_exclusive_group(required=True)
-    inputs.add_argument("-s", dest="studio", nargs="*", help="Studio ID. Will list blocks for all projects from the studio with the given ID.")
     inputs.add_argument("-p", dest="project", nargs="*", help="Project ID. Will list blocks for one project for each ID provided.")
-    inputs.add_argument("-f", dest="studio_list", nargs="*", help="File name for a line-separated list of studio URLs (or IDs). Will list blocks for all projects in all studios.")
-    inputs.add_argument("-g", dest="project_list", nargs="*", help="File name for a line-separated list of project URLs (or IDs). Will list blocks for all projects.")
+    inputs.add_argument("-g", dest="project_list", nargs="*", help="File name for a line-separated list of project IDs. Will list blocks for all projects.")
 
     # Arguments related to output
     parser.add_argument("-d", dest="output_directory", help="Output directory. Will save output to this directory, and create the directory if doesn’t exist.")
